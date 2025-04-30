1db10867a164728d32dc1c6e3090e40e226f468e
dyoJ <sojung10203@gmail.com>
2025-04-30 19:16:38 +0900

    잠깐 커밋

--- src/App.js
+++ src/App.js
@@ -1,33 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import './App.css';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
-import Sidebar from './pages/sidebar/Sidebar';
-import HomePage from './pages/homepage/Homepage';
-import TotalPage from './pages/totalpage/TotalPage';
-
-
-function App() {
-  return (
-      <Router>
-          <div className="App">
-              <div className="container">
-                  <Sidebar />
-                  <div className="others">
-                      <Routes>
-                          <Route path="/" element={<Navigate to="/home" />} />
-                          <Route path="/home" element={<HomePage />} />
-                          <Route path="/statistics/:category" element={<TotalPage />} />
-                          <Route path="/ranking" element={<div>랭킹 페이지</div>} />
-                      </Routes>
-                  </div>
-              </div>
-          </div>
-      </Router>
-
-  );
-}
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useLocation } from "react-router-dom";
 import Sidebar from './pages/sidebar/Sidebar';
 import HomePage from './pages/homepage/Homepage';
