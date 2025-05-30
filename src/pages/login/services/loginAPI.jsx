3f39baeafe1eb675c6053d95c107959d02417285
Narea <skgus6699@naver.com>
2025-05-30 22:36:55 +0900

    [feature/mainpage] fix : 로그인 코드 하드코딩 수정2

--- src/pages/login/services/loginAPI.jsx
+++ src/pages/login/services/loginAPI.jsx
@@ -1,7 +1,6 @@
 import apiClient from 'pages/common/services/apiClient'
 
-//POST 방식
-export const get = async (emailId, password) => {
+export const getAuth = async (emailId, password) => {
     const response = await apiClient.post(`/user/login`, {
         emailId,
         password,
